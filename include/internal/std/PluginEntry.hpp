938e00db60aeebdc8aefcdac79925129a336c258
YellowCat98 <elcucuyisgay@gmail.com>
2026-03-27 21:25:35 +0100

    clarity is important part 8

--- include/internal/std/PluginEntry.hpp
+++ include/internal/std/PluginEntry.hpp
@@ -2,7 +2,7 @@
 #include <internal/SerpentLua.hpp>
 #include <sol/sol.hpp> // since the builtin plugin isnt in internal/SerpentLua.hpp, its no longer a part of the serpentlua core so i can include sol2
 
-namespace SerpentLua::internal::ScriptBuiltin { // builtin plugin, can be included within a script by doing `--@plugins serpentlua_builtin`
+namespace SerpentLua::internal::ScriptBuiltin { // builtin plugin, can be used within a script by adding `--@plugins serpentlua_builtin` to metadata.
 	void entry(lua_State* L);
 
 	inline SerpentLua::Plugin* plugin = nullptr;
