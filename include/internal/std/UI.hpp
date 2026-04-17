d9a610d6d4d34e9f9d373c0f0c5e97d958eb038c
YellowCat98 <elcucuyisgay@gmail.com>
2026-04-17 17:58:51 +0100

    i added the getters

--- include/internal/std/UI.hpp
+++ include/internal/std/UI.hpp
@@ -4,14 +4,25 @@
 #include <internal/std/Enums.hpp>
 #include <sol/sol.hpp>
 
+#define DEFINE_ADD_LAMBDA(name) \
+std::function<void(const std::string&, std::function<sol::object(CCNode*, std::optional<sol::object>)>)> add = [&](const std::string& name, std::function<sol::object(CCNode*, std::optional<sol::object>)> fn) { \
+	node->methods.insert_or_assign(name, ScriptBuiltin::ui::Attribute{ \
+		[=](ScriptBuiltin::ui::Node* node, std::optional<sol::object> value) { \
+			auto ccnode = getCCNode(state, node); \
+			if (!ccnode) return sol::make_object(state, sol::nil); \
+			return fn(ccnode, value); \
+		} \
+	}); \
+}; \
+
 namespace SerpentLua::internal::ScriptBuiltin::ui {
 	sol::table entry(sol::state_view state);
 
 	struct Node;
 	struct Attribute {
-		std::function<sol::object(Node*, sol::object)> apply;
+		std::function<sol::object(Node*, std::optional<sol::object>)> apply;
 
-		Attribute(std::function<sol::object(Node*, sol::object)> apply) : apply(apply) {}
+		Attribute(std::function<sol::object(Node*, std::optional<sol::object>)> apply) : apply(apply) {}
 	};
 
 	// functions that dont take this_state as their first argument dont get bound
@@ -37,13 +48,11 @@ namespace SerpentLua::internal::ScriptBuiltin::ui {
 		ScriptBuiltin::Enums::ui::NodeType type;
 	};
 
-
-
 	struct AttributeHandler {
-		static void populateAttributesNode(sol::state_view ts, Node* node);
-		static void populateAttributesSprite(sol::state_view ts, Node* node);
-		static void populateAttributesButton(sol::state_view ts, Node* node);
-		static void populateAttributesLabel(sol::state_view ts, Node* node);
-		static void populateAttributesMenu(sol::state_view ts, Node* node);
+		static void populateAttributesNode(sol::state_view state, Node* node);
+		static void populateAttributesSprite(sol::state_view state, Node* node);
+		static void populateAttributesButton(sol::state_view state, Node* node);
+		static void populateAttributesLabel(sol::state_view state, Node* node);
+		static void populateAttributesMenu(sol::state_view state, Node* node);
 	};
 }
--- src/internal/std/ui/AttributeHandler.cpp
+++ src/internal/std/ui/AttributeHandler.cpp
@@ -1,12 +1,15 @@
-#include <internal/std/ui.hpp>
+#include <internal/std/UI.hpp>
 #include <internal/std/PluginEntry.hpp>
 
 using namespace SerpentLua::internal;
 using namespace geode::prelude;
 
 template <typename T>
-bool typeCheck(sol::object value, const char* name) {
-	if (!value.is<T>()) {
+bool typeCheck(std::optional<sol::object> value, const std::string& name) {
+	if (!value.has_value()) {
+		log::warn("[UI PLUGIN] {} has no value.", name);
+	}
+	if (!(*value).is<T>()) {
 		log::warn("[UI PLUGIN] {} type mismatch.", name);
 		return false;
 	}
@@ -21,31 +24,23 @@ static CCNode* getCCNode(sol::state_view state, ScriptBuiltin::ui::Node* node) {
 void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view state, Node* node) {
 	if (!node) return;
 
-	std::function<void(const std::string&, std::function<sol::object(CCNode*, sol::object)>)> add = [&](const std::string& name, std::function<sol::object(CCNode*, sol::object)> fn) {
-		node->methods.insert_or_assign(name, ScriptBuiltin::ui::Attribute{
-			[=](ScriptBuiltin::ui::Node* node, sol::object value) {
-				auto ccnode = getCCNode(state, node);
-				if (!ccnode) return sol::make_object(state, sol::nil);
-				return fn(ccnode, value);
-			}
-		});
-	};
+	DEFINE_ADD_LAMBDA(add)
 
-	add("setPositionX", [state](CCNode* ccnode, sol::object value) {
+	add("setPositionX", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setPositionX")) return sol::make_object(state, sol::nil);
-		ccnode->setPositionX(value.as<float>());
+		ccnode->setPositionX((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setPositionY", [state](CCNode* ccnode, sol::object value) {
+	add("setPositionY", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setPositionY")) return sol::make_object(state, sol::nil);
-		ccnode->setPositionY(value.as<float>());
+		ccnode->setPositionY((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setPosition", [state](CCNode* ccnode, sol::object value) {
+	add("setPosition", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<sol::table>(value, "setPosition")) return sol::make_object(state, sol::nil);
-		auto t = value.as<sol::table>();
+		auto t = (*value).as<sol::table>();
 
 		if (!t["x"].is<float>() || !t["y"].is<float>()) {
 			log::warn("[UI PLUGIN] setPosition table missing float x/y.");
@@ -56,21 +51,21 @@ void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setScaleX", [state](CCNode* ccnode, sol::object value) {
+	add("setScaleX", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setScaleX")) return sol::make_object(state, sol::nil);
-		ccnode->setScaleX(value.as<float>());
+		ccnode->setScaleX((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setScaleY", [state](CCNode* ccnode, sol::object value) {
+	add("setScaleY", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setScaleY")) return sol::make_object(state, sol::nil);
-		ccnode->setScaleY(value.as<float>());
+		ccnode->setScaleY((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setScale", [state](CCNode* ccnode, sol::object value) {
+	add("setScale", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<sol::table>(value, "setScale")) return sol::make_object(state, sol::nil);
-		auto t = value.as<sol::table>();
+		auto t = (*value).as<sol::table>();
 
 		if (!t["x"].is<float>() || !t["y"].is<float>()) {
 			log::warn("[UI PLUGIN] setScale table missing float x/y.");
@@ -82,25 +77,25 @@ void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setAnchorPointX", [state](CCNode* ccnode, sol::object value) {
+	add("setAnchorPointX", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setAnchorPointX")) return sol::make_object(state, sol::nil);
 		auto anchor = ccnode->getAnchorPoint();
-		anchor.x = value.as<float>();
+		anchor.x = (*value).as<float>();
 		ccnode->setAnchorPoint(anchor);
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setAnchorPointY", [state](CCNode* ccnode, sol::object value) {
+	add("setAnchorPointY", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setAnchorPointY")) return sol::make_object(state, sol::nil);
 		auto anchor = ccnode->getAnchorPoint();
-		anchor.y = value.as<float>();
+		anchor.y = (*value).as<float>();
 		ccnode->setAnchorPoint(anchor);
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setAnchorPoint", [state](CCNode* ccnode, sol::object value) {
+	add("setAnchorPoint", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<sol::table>(value, "setAnchorPoint")) return sol::make_object(state, sol::nil);
-		auto t = value.as<sol::table>();
+		auto t = (*value).as<sol::table>();
 
 		if (!t["x"].is<float>() || !t["y"].is<float>()) {
 			log::warn("[UI PLUGIN] setAnchorPoint table missing float x/y.");
@@ -111,25 +106,21 @@ void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setContentSizeWidth", [state](CCNode* ccnode, sol::object value) {
-		if (!typeCheck<float>(value, "setContentSizeWidth")) return sol::make_object(state, sol::nil);
-		auto size = ccnode->getContentSize();
-		size.width = value.as<float>();
-		ccnode->setContentSize(size);
+	add("setContentWidth", [state](CCNode* ccnode, std::optional<sol::object> value) {
+		if (!typeCheck<float>(value, "setContentWidth")) return sol::make_object(state, sol::nil);
+		ccnode->setContentWidth((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setContentSizeHeight", [state](CCNode* ccnode, sol::object value) {
-		if (!typeCheck<float>(value, "setContentSizeHeight")) return sol::make_object(state, sol::nil);
-		auto size = ccnode->getContentSize();
-		size.height = value.as<float>();
-		ccnode->setContentSize(size);
+	add("setContentHeight", [state](CCNode* ccnode, std::optional<sol::object> value) {
+		if (!typeCheck<float>(value, "setContentHeight")) return sol::make_object(state, sol::nil);
+		ccnode->setContentHeight((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setContentSize", [state](CCNode* ccnode, sol::object value) {
+	add("setContentSize", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<sol::table>(value, "setContentSize")) return sol::make_object(state, sol::nil);
-		auto t = value.as<sol::table>();
+		auto t = (*value).as<sol::table>();
 
 		if (!t["width"].is<float>() || !t["height"].is<float>()) {
 			log::warn("[UI PLUGIN] setContentSize table missing float width/height.");
@@ -140,21 +131,21 @@ void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setRotationX", [state](CCNode* ccnode, sol::object value) {
+	add("setRotationX", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setRotationX")) return sol::make_object(state, sol::nil);
-		ccnode->setRotationX(value.as<float>());
+		ccnode->setRotationX((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setRotationY", [state](CCNode* ccnode, sol::object value) {
+	add("setRotationY", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<float>(value, "setRotationY")) return sol::make_object(state, sol::nil);
-		ccnode->setRotationY(value.as<float>());
+		ccnode->setRotationY((*value).as<float>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setRotation", [state](CCNode* ccnode, sol::object value) {
+	add("setRotation", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<sol::table>(value, "setRotation")) return sol::make_object(state, sol::nil);
-		auto t = value.as<sol::table>();
+		auto t = (*value).as<sol::table>();
 
 		if (!t["x"].is<float>() || !t["y"].is<float>()) {
 			log::warn("[UI PLUGIN] setRotation table missing float x/y.");
@@ -166,15 +157,103 @@ void ScriptBuiltin::ui::AttributeHandler::populateAttributesNode(sol::state_view
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setVisible", [state](CCNode* ccnode, sol::object value) {
+	add("setVisible", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<bool>(value, "setVisible")) return sol::make_object(state, sol::nil);
-		ccnode->setVisible(value.as<bool>());
+		ccnode->setVisible((*value).as<bool>());
 		return sol::make_object(state, sol::nil);
 	});
 
-	add("setID", [state](CCNode* ccnode, sol::object value) {
+	add("setID", [state](CCNode* ccnode, std::optional<sol::object> value) {
 		if (!typeCheck<std::string>(value, "setID")) return sol::make_object(state, sol::nil);
-		ccnode->setID(value.as<std::string>());
+		ccnode->setID((*value).as<std::string>());
 		return sol::make_object(state, sol::nil);
+	});
+
+
+
+	add("getPositionX", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getPositionX());
+	});
+
+	add("getPositionY", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getPositionY());
+	});
+
+	add("getPosition", [state](CCNode* ccnode, std::optional<sol::object>) {
+		auto pos = ccnode->getPosition();
+		auto t = sol::state_view(state).create_table();
+		t["x"] = pos.x;
+		t["y"] = pos.y;
+		return sol::make_object(state, t);
+	});
+
+	add("getScaleX", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getScaleX());
+	});
+
+	add("getScaleY", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getScaleY());
+	});
+
+	add("getScale", [state](CCNode* ccnode, std::optional<sol::object>) {
+		auto t = sol::state_view(state).create_table();
+		t["x"] = ccnode->getScaleX();
+		t["y"] = ccnode->getScaleY();
+		return sol::make_object(state, t);
+	});
+
+	add("getAnchorPointX", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getAnchorPoint().x);
+	});
+
+	add("getAnchorPointY", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getAnchorPoint().y);
+	});
+
+	add("getAnchorPoint", [state](CCNode* ccnode, std::optional<sol::object>) {
+		auto anchor = ccnode->getAnchorPoint();
+		auto t = sol::state_view(state).create_table();
+		t["x"] = anchor.x;
+		t["y"] = anchor.y;
+		return sol::make_object(state, t);
+	});
+
+	add("getContentWidth", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getContentWidth());
+	});
+
+	add("getContentHeight", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getContentHeight());
+	});
+
+	add("getContentSize", [state](CCNode* ccnode, std::optional<sol::object>) {
+		auto size = ccnode->getContentSize();
+		auto t = sol::state_view(state).create_table();
+		t["width"] = size.width;
+		t["height"] = size.height;
+		return sol::make_object(state, t);
+	});
+
+	add("getRotationX", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getRotationX());
+	});
+
+	add("getRotationY", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->getRotationY());
+	});
+
+	add("getRotation", [state](CCNode* ccnode, std::optional<sol::object>) {
+		auto t = sol::state_view(state).create_table();
+		t["x"] = ccnode->getRotationX();
+		t["y"] = ccnode->getRotationY();
+		return sol::make_object(state, t);
+	});
+
+	add("isVisible", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, ccnode->isVisible());
+	});
+
+	add("getID", [state](CCNode* ccnode, std::optional<sol::object>) {
+		return sol::make_object(state, std::string(ccnode->getID()));
 	});
 }
