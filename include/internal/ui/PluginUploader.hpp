a09666a24beb82cdac424160364a46c42ffffcac
YellowCat98 <elcucuyisgay@gmail.com>
2026-06-11 17:02:55 +0100

    added a bunch o functions

--- include/internal/ui/PluginUploader.hpp
+++ include/internal/ui/PluginUploader.hpp
@@ -6,9 +6,14 @@
 namespace SerpentLua::internal::ui {
 	class PluginUploader : public geode::Popup {
 	protected:
-		geode::async::TaskHolder<geode::utils::web::WebResponse> m_listener;
+		geode::async::TaskHolder<geode::utils::web::WebResponse> m_Listener;
 		bool init();
+
+		// i dont think neither of these functions would be small enough to just shove into a lambda with ccmenuitemext
 		void onUpload(cocos2d::CCObject*);
+		void onUploadExample(cocos2d::CCObject*);
+		void onImportDesc(cocos2d::CCObject*);
+		// unfortunately youll have to import your description.md file because i am NOT going to use imgui so you can write multiline
 	public:
 		static PluginUploader* create();
 	};
