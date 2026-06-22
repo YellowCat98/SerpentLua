b8538f9674d1100f5ea42af35744624daa1aca6d
YellowCat98 <elcucuyisgay@gmail.com>
2026-06-22 23:09:10 +0100

    added page system now you can view all your owned plugins!

--- include/internal/ui/OwnPluginManager.hpp
+++ include/internal/ui/OwnPluginManager.hpp
@@ -7,11 +7,16 @@ namespace SerpentLua::internal::ui {
 	class OwnPluginManager : public geode::Popup {
 	protected:
 		geode::async::TaskHolder<geode::utils::web::WebResponse> m_listener;
-		bool init();
 		geode::MDTextArea* md;
+		CCMenuItemSpriteExtra* nextBtn;
+		CCMenuItemSpriteExtra* backBtn;
 		std::vector<std::string> prettyStrings;
+		int currentPage;
+		bool init();
 		std::string createPrettyPluginInfo(const DisplayInfo& info);
 		void updateMD();
+		void loadPage(int page);
+		void movePage(cocos2d::CCObject*);
 	public:
 		static OwnPluginManager* create();
 	};
--- src/internal/ui/OwnPluginManager.cpp
+++ src/internal/ui/OwnPluginManager.cpp
@@ -8,6 +8,29 @@ bool OwnPluginManager::init() {
 
 	// oh COME ON i dont want to do layout bs again
 
+	auto arrowMenu = CCMenu::create();
+	arrowMenu->setID("arrow-menu");
+	arrowMenu->setContentSize({m_size.width + 100.0f, m_size.height});
+	arrowMenu->setPosition(m_size / 2);
+	arrowMenu->setLayout(
+		SimpleRowLayout::create()
+		->setMainAxisAlignment(MainAxisAlignment::Center)
+		->setMainAxisDirection(AxisDirection::RightToLeft)
+		->setGap(m_size.width + 20.0f)
+	);
+
+	nextBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"), this, menu_selector(OwnPluginManager::movePage));
+	nextBtn->setTag(1);
+	nextBtn->getNormalImage()->setScaleX(-1.0f);
+	nextBtn->setID("arrow-next");
+
+	backBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"), this, menu_selector(OwnPluginManager::movePage));
+	backBtn->setTag(-1);
+	backBtn->setID("arrow-next");
+
+	arrowMenu->addChild(nextBtn);
+	arrowMenu->addChild(backBtn);
+
 	auto layoutMenu = CCMenu::create();
 	layoutMenu->setID("layout-menu");
 	layoutMenu->setContentSize(m_size);
@@ -33,36 +56,14 @@ bool OwnPluginManager::init() {
 
 	layoutMenu->addChild(details);
 
+	arrowMenu->updateLayout();
 	layoutMenu->updateLayout();
 	details->updateLayout();
 
+	m_mainLayer->addChild(arrowMenu);
 	m_mainLayer->addChild(layoutMenu);
 
-	auto req = ServerManager::get()->createReq();
-	req.param("sort", "most_recent");
-	req.param("account_id", GJAccountManager::get()->m_accountID);
-	req.param("page", 1);
-
-	m_listener.spawn(ServerManager::get()->sendReq("GET", "/api/v1/plugin/fetch/bulk", req), [this](web::WebResponse resp) {
-		if (!(resp.code() >= 200 && resp.code() < 300)) {
-			this->md->setString(fmt::format("Error: {} (Code {})", resp.string().unwrap(), resp.code()).c_str());
-			return;
-		}
-
-		auto jsonRes = resp.json();
-		if (jsonRes.isErr()) {
-			this->md->setString(fmt::format("JSON error: {}", jsonRes.unwrapErr()).c_str());
-			return;
-		}
-
-		auto json = jsonRes.unwrap();
-		// there we go
-
-		for (const auto& item : json["items"].asArray().unwrap()) {
-			this->prettyStrings.push_back(this->createPrettyPluginInfo(DisplayInfo::create(item)));
-		}
-		this->updateMD();
-	});
+	loadPage(1);
 	return true;
 }
 
@@ -90,6 +91,54 @@ void OwnPluginManager::updateMD() {
 	}
 
 	this->md->setString(ss.str().c_str());
+}
+
+void OwnPluginManager::loadPage(int page) {
+	currentPage = page;
+
+	nextBtn->setVisible(false);
+	backBtn->setVisible(false);
+
+	this->prettyStrings = {};
+	md->setString(fmt::format("Loading Page: {}", currentPage).c_str());
+
+	auto req = ServerManager::get()->createReq();
+	req.param("sort", "most_recent");
+	req.param("account_id", 67);
+	req.param("page", currentPage);
+
+	m_listener.spawn(ServerManager::get()->sendReq("GET", "/api/v1/plugin/fetch/bulk", req), [this](web::WebResponse resp) {
+		if (!(resp.code() >= 200 && resp.code() < 300)) {
+			this->md->setString(fmt::format("Error: {} (Code {})", resp.string().unwrap(), resp.code()).c_str());
+			return;
+		}
+
+		auto jsonRes = resp.json();
+		if (jsonRes.isErr()) {
+			this->md->setString(fmt::format("JSON error: {}", jsonRes.unwrapErr()).c_str());
+			return;
+		}
+
+		auto json = jsonRes.unwrap();
+		// there we go
+
+		// first tings first we do da arrow stuff
+
+		bool hasNext = json["has_next"].asBool().unwrap();
+		bool hasPrev = json["has_prev"].asBool().unwrap();
+
+		this->nextBtn->setVisible(hasNext);
+		this->backBtn->setVisible(hasPrev);
+
+		for (const auto& item : json["items"].asArray().unwrap()) {
+			this->prettyStrings.push_back(this->createPrettyPluginInfo(DisplayInfo::create(item)));
+		}
+		this->updateMD();
+	});
+}
+
+void OwnPluginManager::movePage(CCObject* sender) {
+	this->loadPage(currentPage + sender->getTag());
 }
 
 OwnPluginManager* OwnPluginManager::create() {
