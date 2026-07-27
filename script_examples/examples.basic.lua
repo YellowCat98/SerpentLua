a85c1a73632a1a5d9ae96d7ad9cd8b2047131e4a
YellowCat98 <elcucuyisgay@gmail.com>
2026-07-27 16:46:37 +0100

    document fmt

--- script_examples/examples.basic.lua
+++ script_examples/examples.basic.lua
@@ -11,21 +11,24 @@ local SL = require("serpentlua.std")
 local metadata = SL.ScriptMetadata.get()
 --[[
 	SL.ScriptMetadata: Represents the metadata of a script.
-	memb: name, id, developer, serpentVersion, version, nostd, plugins
+	members: name, id, developer, serpentVersion, version, nostd, plugins
 ]]
 SL.log.info("Hello, World!")
 
-SL.log.debug(string.format("Beep Boop! Script %s (ID: %s) is running on version %s <insert more robot noises>", metadata.name, metadata.id, metadata.version))
+SL.log.debug(SL.fmt.format("Beep Boop! Script {} (ID: {}) is running on version {} <insert more robot noises>", metadata.name, metadata.id, metadata.version))
+-- SL.fmt.format uses the same syntax as FMT library for the C++ language.
+-- What differentiates it from `string.format` is that you do not need to know the type of the format arguments inside the format string.
 
 local stringToPlayAroundWith = "Hello, World."
 
-SL.log.debug(string.format("ANALYSING STRING: %s", stringToPlayAroundWith))
-SL.log.debug(string.format("LENGTH: %d", string.len(stringToPlayAroundWith)))
-SL.log.debug(string.format("IN UPPERCASE: %s", string.upper(stringToPlayAroundWith)))
+SL.log.debug(SL.fmt.format("ANALYSING STRING: {}", stringToPlayAroundWith))
+SL.log.debug(SL.fmt.format("LENGTH: {}", string.len(stringToPlayAroundWith)))
+SL.log.debug(SL.fmt.format("IN UPPERCASE: {}", string.upper(stringToPlayAroundWith)))
+SL.log.debug(SL.fmt.format("IN LOWERCASE: {}", string.lower(stringToPlayAroundWith)))
 
 SL.log.warn("There's nothing to warn for...? but this could be useful too.")
 SL.log.error("There is no error, yet. This function will be useful in the future.")
-SL.log.trace("This function is very useful.")
+SL.log.trace("Wow! Super Duper Verbose Message.")
 
 --[[
 SL.log: A table containing 4 logging functions.
@@ -43,7 +46,7 @@ local plugin = SL.PluginMetadata.getByID("serpentlua.std")
 	Note that this is not exclusive to just plugins this script has loaded, but every plugin. Note that plugins that have failed do not get recognized.
 ]]
 
-SL.log.info(string.format("The plugin responsible for logging this message is of ID \"%s\".", plugin.id))
+SL.log.info(SL.fmt.format("The plugin responsible for logging this message is of ID \"{}\".", plugin.id))
 
 -- SL.ScriptMetadata.plugins returns an array of strings, not PluginMetadata object, you must get the PluginMetadata yourself through the id.
 for k, v in pairs(metadata.plugins) do
